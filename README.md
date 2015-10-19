d2db9bc2be17b287008e07619c5429f3211dbd0d
Jesse Plamondon-Willard <jesse.plamondon@guestdriven.com>
2015-10-19 16:07:38 -0400

    add module for tokenising text payloads

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# webscript-helpers
-A collection of generic scripts for writing Webscript.io webhooks.
+A collection of simple Lua modules for writing Webscript.io webhooks
+(see _[Including Modules From GitHub](https://www.webscript.io/documentation#modules)_).
+
+### Tokeniser
+This module extracts tokens from arbitrary text. It's mainly intended for
+webhooks that need to parse text payloads.
+
+For example, Amazon Web Services sends alert emails that look like this:
+> **AWS Elastic Beanstalk Notification - New application version was deployed to running EC2 instances**
+> 
+> Timestamp: Fri Oct 16 15:40:00 UTC 2015  
+> Message: New application version was deployed to running EC2 instances.  
+> Environment: sample-api-edge  
+> Application: sample-api  
+> Environment URL: http://sample-api-edge.elasticbeanstalk.com
+
+This module lets you tokenise simple strings like the subject:
+
+```lua
+local service, subject = tokeniser.getCaptures(payload.subject, "^AWS ([^-]+) Notification - (.+)")
+```
+
+Or tokenise complex strings:
+
+```lua
+local tokens = {}
+tokeniser.parse(tokens, payload.body, {
+   timestamp = "Timestamp: ([^\r\n]+)",
+   message = "Message: ([^\r\n]+)",
+   environment = "Environment: ([^\r\n]+)",
+   environmentUrl = "Environment URL: ([^\r\n]+)",
+   application = "Application: ([^\r\n]+)"
+})
+```
+
+(See the docstrings in the Lua file for details.)
---
+++ tokeniser.lua
@@ -0,0 +1,56 @@
+local string, table, pairs = string, table, pairs
+
+--[[
+This module extracts tokens from raw text based on the specified patterns. It's
+mainly intended for webhooks that need to parse incoming payloads (e.g. parsing
+the details from an Amazon Web Services email alert).
+]]
+module('tokeniser')
+_DESCRIPTION = 'Text payload tokeniser'
+_VERSION = 'tokeniser 0.1'
+
+--[[
+getCaptures
+
+Returns substrings matched by capturing groups in a regex pattern. This is
+equivalent to the values returned by string.find without the start & end
+indexes.
+
+Parameters
+	text: The text to search.
+	pattern: The regex pattern containing capturing groups.
+
+Example:
+	local username, messages = tokeniser.getCaptures("Hi Bob! You have 42 points.", "Hi ([^!]+)! You have (\d+) points")
+]]
+function getCaptures(text, pattern)
+	local values = {string.find(subject, pattern)}
+	table.remove(values, 1)
+	table.remove(values, 1)
+	return unpack(values)
+end
+
+--[[
+parseTokens
+
+Extracts tokens from a raw text (based on the specified patterns) and adds them
+to the provided table. If the table already contains a matched token, it will
+be overwritten.
+
+Parameters
+	tokens: A table to populate with tokens.
+	text: The text to parse.
+	map: A table indicating how to parse the message. The keys should be the
+		token names, and the values should be a regex pattern whose first
+		capture contains the token value.
+
+Example:
+	local tokens = {}
+	tokeniser.parseTokens(tokens, "Service: Amazon Beanstalk\nError: something went very wrong.", { service = "Service: ([^\n]+)", error = "Error: ([^\n]+)" })
+]]
+function parse(tokens, text, map)
+	for name, pattern in pairs(map) do
+		local _, _, value = string.find(text, pattern)
+		tokens[name] = value or ''
+	end
+end
